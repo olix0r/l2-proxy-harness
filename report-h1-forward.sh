62870dd0964a9e1b243df860cc1322a9e263ea0e
Oliver Gould <ver@buoyant.io>
2020-04-06 22:14:58 +0000

    fixup grpc

---
+++ report-h1-forward.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eu
+
+cd "${BASH_SOURCE[0]%/*}"
+source ./inc.harness.sh
+
+server_id=$(server_create)
+
+export PROXY_DST_SUFFIXES=""
+proxy_id=$(proxy_create)
+
+export CLIENT_TARGET_HOST=h1-forward.test.example.com
+mock_dst_id=$(mock_dst_create)
+
+start "$server_id" "$proxy_id" "$mock_dst_id"
+trap '{ stop "$server_id" "$proxy_id" "$mock_dst_id"; }' EXIT
+proxy_await
+
+# Warmup
+curl -so /dev/null \
+    -H "Host: $CLIENT_TARGET_HOST" \
+    "http://127.0.0.1:$PROXY_OUTBOUND_PORT"
+
+client_run_proxy_report "h1-forward"
--- report-h2-grpc-forward.sh
+++ report-h2-grpc-forward.sh
@@ -8,18 +8,19 @@ source ./inc.harness.sh
 server_id=$(server_create)
 
 export PROXY_INBOUND_ORIG_DST_PORT="$SERVER_GRPC_PORT"
+export PROXY_DST_SUFFIXES=""
 proxy_id=$(proxy_create)
 
-export CLIENT_TARGET_HOST=h2-grpc.test.example.com
-export MOCK_DST_ENDPOINTS="${CLIENT_TARGET_HOST}:80=127.0.0.1:$PROXY_INBOUND_PORT#h2"
 mock_dst_id=$(mock_dst_create)
 
 start "$server_id" "$proxy_id" "$mock_dst_id"
 trap '{ stop "$server_id" "$proxy_id" "$mock_dst_id"; }' EXIT
 proxy_await
 
+export CLIENT_TARGET_HOST=h2-grpc-forward.test.example.com
+
 # Warmup
-(TOTAL_REQUESTS=1 client_run_proxy_report "h2-grpc" -grpc)
+(TOTAL_REQUESTS=1 client_run_proxy_report "h2-grpc-forward" -grpc)
 
 # Run again with the proper TOTAL_REQUESTS, overwriting the first report.
-client_run_proxy_report "h2-grpc" -grpc
+client_run_proxy_report "h2-grpc-forward" -grpc
