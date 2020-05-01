52c3bd88f4c8041b1a53cb3ed618a43db26948d6
Oliver Gould <ver@buoyant.io>
2020-05-01 00:19:19 +0000

    add a stall test

---
+++ stall.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -eu
+
+cd "${BASH_SOURCE[0]%/*}"
+source ./inc.harness.sh
+
+server_id=$(server_create)
+proxy_id=$(proxy_create)
+
+export CLIENT_TARGET_HOST=h1-via-h2.test.example.com
+export MOCK_DST_ENDPOINTS="${CLIENT_TARGET_HOST}:80=127.0.0.1:$PROXY_INBOUND_PORT#h2"
+mock_dst_id=$(mock_dst_create)
+
+start "$server_id" "$proxy_id" "$mock_dst_id"
+trap '{ stop "$server_id" "$proxy_id" "$mock_dst_id"; }' EXIT
+proxy_await
+
+curl -so /dev/null \
+    -H "Host: $CLIENT_TARGET_HOST" \
+    "http://127.0.0.1:$PROXY_OUTBOUND_PORT"
+
+sleep 62
+
+curl -vso /dev/null \
+    -H "Host: $CLIENT_TARGET_HOST" \
+    "http://127.0.0.1:$PROXY_OUTBOUND_PORT"
+
+#docker logs "$proxy_id"
