60a5641365894dcc53b29dd455b69b35536cb3c4
Oliver Gould <ver@buoyant.io>
2020-05-07 15:24:10 +0000

    wip

--- hey.sh
+++ hey.sh
@@ -6,7 +6,8 @@ cd "${BASH_SOURCE[0]%/*}"
 source ./inc.harness.sh
 
 server_id=$(server_create)
-export PROXY_LOG=linkerd=trace,hyper=trace,info
+
+#export PROXY_IMAGE=olix0r/l2-proxy:v2.95.0-mock-orig-dst
 proxy_id=$(proxy_create)
 
 export CLIENT_TARGET_HOST=h1.test.example.com
@@ -17,7 +18,8 @@ start "$server_id" "$proxy_id" "$mock_dst_id"
 trap '{ stop "$server_id" "$proxy_id" "$mock_dst_id"; }' EXIT
 proxy_await
 
-hey -z 10s -c 500 -q 1 -host "$CLIENT_TARGET_HOST" "http://127.0.0.1:$PROXY_INBOUND_PORT"
+echo ":;" hey -z 10s -q 500 -c 1000 -host "$CLIENT_TARGET_HOST" "http://127.0.0.1:$PROXY_OUTBOUND_PORT"
+hey -z 10s -q 500 -c 1000 -host "$CLIENT_TARGET_HOST" "http://127.0.0.1:$PROXY_OUTBOUND_PORT"
 
 mkdir -p target
 proxy_metrics >target/hey.metrics
