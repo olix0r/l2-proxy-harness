ea382737bf558d8382294024e2ecf04ed46befe7
Oliver Gould <ver@buoyant.io>
2020-07-08 23:12:56 +0000

    more
    scripts

--- h2-outbound.sh
+++ h2-outbound.sh
@@ -5,10 +5,10 @@ set -eu
 cd "${BASH_SOURCE[0]%/*}"
 source ./inc.harness.sh
 
-CLIENT_TARGET_HOST=h1.test.example.com:8080
-MOCK_DST_ENDPOINTS="${CLIENT_TARGET_HOST}=127.0.0.1:$SERVER_HTTP_PORT"
+CLIENT_TARGET_HOST="127.0.0.1:${PROXY_OUTBOUND_PORT}"
+MOCK_DST_ENDPOINTS="${CLIENT_TARGET_HOST}=127.0.0.1:$SERVER_GRPC_PORT"
 PROXY_DST_NETWORKS="127.0.0.1/32"
-PROXY_INBOUND_ORIG_DST_PORT="$SERVER_HTTP_PORT"
+PROXY_INBOUND_ORIG_DST_PORT="$SERVER_GRPC_PORT"
 
 server_id=$(server_create)
 mock_dst_id=$(mock_dst_create)
@@ -19,8 +19,5 @@ proxy_await
 
 CONCURRENCY="${CONCURRENCY:-1}"
 
-# Warmup
-curl -so /dev/null \
-    -H "Host: $CLIENT_TARGET_HOST" \
-    "http://127.0.0.1:$PROXY_OUTBOUND_PORT"
-client_run_proxy_report "x${CONCURRENCY}.h1" -quiet
+(TOTAL_REQUESTS=1 client_run_proxy_report "x${CONCURRENCY}.h2" -grpc)
+client_run_proxy_report "x${CONCURRENCY}.h2" -grpc -quiet
